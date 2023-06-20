1f7f14c08f1895203a163f7717594f1c4c7dfe57
Cauã Arseno <cauaarseno61@gmail.com>
2023-06-19 23:27:08 -0300

    Criando Seletores

---
+++ Javascript/Seletores/js2.js
@@ -0,0 +1,8 @@
+function funcaoTeste() {
+    //alert('Estou Aqui')
+    document.getElementById("box1").style.backgroundColor = "blue";
+    document.getElementById("box1").style.textAlign = "center";
+    document.getElementById("box1").style.color = "white";
+
+    document.getElementsByClassName('paragrafo')[0].style.backgroundColor = "red";
+}
