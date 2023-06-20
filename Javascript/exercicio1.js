a758f36148cea7597f8e9d2b6c703fa447167a1f
Cauã Arseno <cauaarseno61@gmail.com>
2023-06-19 22:38:42 -0300

    Primeiro texto vindo de uma função

--- Javascript/exercicio1.js
+++ Javascript/exercicio1.js
@@ -1,3 +1,4 @@
 function exibirAlerta() {
-    alert('Meu primeiro alerta usando um arquivo externo')
+    //alert('Meu primeiro alerta usando um arquivo externo')
+    document.getElementById('box').innerHTML = 'Meu primeiro texto vindo de uma função'
 }
