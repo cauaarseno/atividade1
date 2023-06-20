b480d51f6d06a73db33e99c96305991599fd1bba
Cauã Arseno <cauaarseno61@gmail.com>
2023-06-19 22:49:45 -0300

    primeiro confirm

--- Javascript/exercicio1.js
+++ Javascript/exercicio1.js
@@ -1,6 +1,6 @@
 function exibirAlerta() {
     //alert('Meu primeiro alerta usando um arquivo externo')
     //document.getElementById('box').innerHTML = 'Meu primeiro texto vindo de uma função'
-
-    console.log('Minha primeira mensagem no console')
+    //console.log('Minha primeira mensagem no console')
+    confirm("Você concorda com isso?");
 }
