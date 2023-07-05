c2bfd14e9fdd00e00a0aa00199d62e3ceace80f6
Cauã Arseno <cauaarseno61@gmail.com>
2023-07-04 22:43:12 -0300

    var e comandos

---
+++ Javascript/Udemy/Variáveis/variaveis.js
@@ -0,0 +1,13 @@
+//string
+var texto = 'Curso de JavaScript'
+
+//number
+    var numeroInteiro  = -7 //int
+    var numeroFracionado = 10.75 //float
+
+//boolean
+var teste = false //true -> verdadeiro / false -> falso
+
+//alert(numeroFracionado)
+//document.write(texto)
+console.log(numeroInteiro)
