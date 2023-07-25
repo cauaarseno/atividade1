3ae5f4df2f8c44b0aa13847d2fd70b29f6ea7e36
Cauã Arseno <cauaarseno61@gmail.com>
2023-07-24 21:53:27 -0300

    Concatenação

---
+++ Javascript/Udemy/Concatenacao/conca.js
@@ -0,0 +1,6 @@
+var nome = prompt('Digite seu nome: ')
+var idade = prompt('Digite sua idade: ')
+var idade2 = 50
+console.log(idade)
+console.log(idade2)
+document.write('<h1> Olá ' + nome +', tudo bem? Você tem '+ idade +' anos, certo?</h1>')
