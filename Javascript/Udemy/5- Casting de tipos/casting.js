6d4ae36d60efc6deee961c01797de4039781ab0c
Cauã Arseno <cauaarseno61@gmail.com>
2023-08-01 23:23:03 -0300

    Casting de tipos

---
+++ Javascript/Udemy/5- Casting de tipos/casting.js
@@ -0,0 +1,11 @@
+//var variavel1 = parseInt(prompt('Digite algum número: '))
+//var variavel2 = parseFloat(prompt('Digite outro número: '))
+//numero: 77.632
+//console.log(variavel1) //77  (por ser Int)
+//console.log(variavel2) //77.632 (por ser Float)
+
+var variavel3 = 10
+var variavel4 = 20
+
+//document.write(variavel1 + variavel2)
+document.write(variavel3.toString() + variavel4.toString()) //ocorrerá a concatenação   
