082a068e2554f6885cdb1d112c62d4f283265791
Cauã Arseno <cauaarseno61@gmail.com>
2023-08-01 22:49:57 -0300

    Media Aritmética

---
+++ Javascript/Udemy/4- Operadores de comparação (condicionais)/ifelse_operadores_condicionais.js
@@ -0,0 +1,10 @@
+var nota1 = Number(prompt('Nota 1: '))
+var nota2 = Number(prompt('Nota 2: '))
+var media = (nota1 + nota2) / 2
+document.write('Primeira nota: ' +nota1+ ' Segunda nota: '+nota2+' Média: '+media+'<br>') 
+if (media >= 7) {
+    document.write('Você passou de ano!!')
+}else {
+    document.write('Você não passou de ano.')
+}
+console.log(nota2)
