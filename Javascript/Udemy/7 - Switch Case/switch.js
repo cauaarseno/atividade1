a2e98869abf014c4d3c477645d625447b8957a7a
Cauã Arseno <cauaarseno61@gmail.com>
2023-08-14 23:28:39 -0300

    Switch Case

---
+++ Javascript/Udemy/7 - Switch Case/switch.js
@@ -0,0 +1,14 @@
+var parametro = parseInt(prompt('Digite um número: '))
+
+switch (parametro) {
+    case 1:
+        document.write('Parâmetro 1')
+        break
+    
+    case 2:
+        document.write('Parâmetro 2')
+        break
+    
+    default:
+        document.write('Default')
+}
