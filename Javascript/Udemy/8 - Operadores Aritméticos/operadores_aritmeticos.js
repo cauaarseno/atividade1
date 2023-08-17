3ee56753ae7d4ca159c6fbb1099ab8ecbbd71ecc
Cauã Arseno <cauaarseno61@gmail.com>
2023-08-16 23:51:02 -0300

    Operadores Aritméticos

---
+++ Javascript/Udemy/8 - Operadores Aritméticos/operadores_aritmeticos.js
@@ -0,0 +1,10 @@
+var num1 = parseInt(prompt('Digite o Valor de num1'))
+var num2 = parseInt(prompt('Digite o Valor de num2'))
+
+document.write('A soma entre '+num1+ ' e ' +num2+ ' é: ' + (num1 + num2) + '<br>')
+document.write('A subtração entre '+num1+ ' e ' +num2+ ' é: ' + (num1 - num2) + '<br>')
+document.write('A multiplicação entre '+num1+ ' e ' +num2+ ' é: ' + (num1 * num2) + '<br>')
+document.write('A divisão entre '+num1+ ' e ' +num2+ ' é: ' + (num1 / num2) + '<br>')
+document.write('O módulo entre '+num1+ ' e ' +num2+ ' é: ' + (num1 % num2) + '<br>')
+document.write('O incremento de '+num1+' é: ' + (++num1) + '<br>')
+document.write('O decremento de '+num1+' é: ' + (--num1) + '<br>')
