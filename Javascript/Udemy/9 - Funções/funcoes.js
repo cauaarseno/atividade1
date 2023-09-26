0675be2ee6f98902d139b975e8007b0df26a93c3
Cauã Arseno <cauaarseno61@gmail.com>
2023-09-25 23:34:24 -0300

    Flexibilidade de parâmetros

--- Javascript/Udemy/9 - Funções/funcoes.js
+++ Javascript/Udemy/9 - Funções/funcoes.js
@@ -8,3 +8,14 @@ let largura = parseFloat(prompt('Insira a largura do terreno:'))
 let comprimento = parseFloat(prompt('Insira o comprimento do terreno:'))
 
 document.write('O terreno possui ' +calcularAreaTerreno(largura, comprimento)+ ' metros quadrados.')
+
+
+/*function soma(a, b) {
+    return a + b
+}
+
+console.log(soma(7, 7))
+console.log(soma(7, 7, 9, 15)) //desconsidera os parâmetros adicionais
+console.log(soma(7)) // a variável faltando será um undefined resultando um not a number (NaN)
+console.log(soma()) //undefined + undefined = not a number
+*/
